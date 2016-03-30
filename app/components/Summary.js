46116014b5927a068886e7d2158aa9fd403e8421
Lee Crosby <lee.marreros@pucp.pe>
2016-03-29 21:02:56 -0700

    [Feature] Adds styling to summary page

--- app/components/Summary.js
+++ app/components/Summary.js
@@ -52,105 +52,128 @@ export default class Summary extends React.Component {
           if (person.gender == 'female') this.femaleQ++;
       })
   }
+  
+  housePage() {
+   console.log(this.props.navigator.getCurrentRoutes());   
+  }
 
   render() {
     return (
-      <ScrollView
-        scrollEventThrottle={200}
-        style={styles.scrollView}>
-                
-        <View style={styles.wrapperContent}>
+      <View style={{flex: 1}}>
+        <ScrollView
+            scrollEventThrottle={200}
+            style={styles.scrollView}>
+                    
+            <View style={styles.wrapperContent}>
 
-            <View style={styles.houseData}>
-                <View style={styles.leftSide}>
-                    <Image source={require('../img/house-icon.png')}/>
-                </View>
-                <View style={styles.rightSide}>
-                    <Text style={[styles.textHouse, {fontSize: 22}]}>{this.state.address}</Text>
-                    <Text style={styles.textHouse}>{this.state.city}, {this.state.state} - {this.state.zipcode}</Text>
-                    <Text style={styles.textHouse}>{this.state.bedrooms} bedrooms</Text>
-                </View>
-            </View>
-            
-            <View style={styles.peopleData}>
-            
-                <View style={styles.rowPeople}>
+                <View style={styles.houseData}>
                     <View style={styles.leftSide}>
-                        <Image source={require('../img/people-icon.png')}/>
+                        <Image source={require('../img/house-icon.png')}/>
                     </View>
                     <View style={styles.rightSide}>
-                        <Text style={styles.textHouse}>{this.state.people.length} PEOPLE</Text>
+                        <Text style={[styles.textHouse, {fontSize: 22}]}>{this.state.address}</Text>
+                        <Text style={styles.textHouse}>{this.state.city}, {this.state.state} - {this.state.zipcode}</Text>
+                        <Text style={styles.textHouse}>{this.state.bedrooms} bedrooms</Text>
                     </View>
                 </View>
                 
-                <View style={styles.rowGender}>
-                    <View style={styles.rowGenderSides}>
-                        <View style={[styles.circle, {backgroundColor: '#FF4BF4'}]}></View>
-                        <Text style={styles.genderText}>{this.femaleQ} female</Text>
+                <View style={styles.peopleData}>
+                
+                    <View style={styles.rowPeople}>
+                        <View style={styles.leftSide}>
+                            <Image source={require('../img/people-icon.png')}/>
+                        </View>
+                        <View style={styles.rightSide}>
+                            <Text style={[styles.textHouse, {fontSize: 22}]}>{this.state.people.length} PEOPLE</Text>
+                        </View>
                     </View>
                     
-                    <View style={styles.rowGenderSides}>
-                        <View style={[styles.circle, {backgroundColor: '#2D2BE6'}]}></View>
-                        <Text style={styles.genderText}>{this.maleQ} male</Text>
+                    <View style={styles.rowGender}>
+                        <View style={styles.rowGenderSides}>
+                            <View style={[styles.circle, {backgroundColor: '#FF4BF4'}]}></View>
+                            <Text style={styles.genderText}>{this.femaleQ} female</Text>
+                        </View>
+                        
+                        <View style={styles.rowGenderSides}>
+                            <View style={[styles.circle, {backgroundColor: '#2D2BE6'}]}></View>
+                            <Text style={styles.genderText}>{this.maleQ} male</Text>
+                        </View>
                     </View>
+                    
+                    <View style={styles.rowsPeopleDetail}>
+                        {this.state.people.map((person, i)=>{
+                            var nameAge = `${person.firstname} ${person.lastname}, ${person.age}`;
+                            var female = person.gender == 'female' ? (true) : (false);
+                            return (
+                                <View key={i} style={styles.wrapDetail}>
+                                    <View style={styles.leftDetail}>
+                                        <View style={[styles.bulletGender, female ? styles.bulletF : null]}></View>
+                                    </View>
+                                    <View style={styles.rightDetail}>
+                                        <Text style={styles.nameage}>{nameAge}</Text>
+                                        <Text style={styles.email}>{person.email}</Text>
+                                    </View>
+                                </View>
+                            );
+                        })}
+                    </View>
+                    
+                </View> 
+                
+                <View style={styles.carsData}>
+                
+                    <View style={styles.rowPeople}>
+                        <View style={styles.leftSide}>
+                            <Image 
+                                style={styles.carIcon} 
+                                source={require('../img/vehicle-icon.png')}/>
+                        </View>
+                        <View style={styles.rightSide}>
+                            <Text style={[styles.textHouse, {fontSize: 22}]}>{this.state.cars.length} CARS</Text>
+                        </View>
+                    </View>             
+                    
+                    <View style={styles.rowsPeopleDetail}>
+                        {this.state.cars.map((car, i)=>{
+                            var firstline = `${car.make}, ${car.model}`;
+                            var secondline = `${car.owner}, ${car.year}`
+                            return (
+                                <View key={i} style={styles.wrapDetail}>
+                                    <View style={styles.leftDetail}>
+                                        <View style={styles.bulletGender}></View>
+                                    </View>
+                                    <View style={styles.rightDetail}>
+                                        <Text style={styles.nameage}>{firstline}</Text>
+                                        <Text style={styles.email}>{secondline}</Text>
+                                    </View>
+                                </View>
+                            );
+                        })}
+                    </View>
+                    
                 </View>
                 
-                <View style={styles.rowsPeopleDetail}>
-                    {this.state.people.map((person, i)=>{
-                        var name = person.firstname + person.lastname;
-                        return (
-                            <View key={i}>
-                                <View>
-                                    <View style={styles.bulletGender}></View>
-                                </View>
-                                <View>
-                                    <Text>{name}</Text>
-                                    <Text>{person.email}</Text>
-                                </View>
-                            </View>
-                        );
-                    })}
-                </View>
-                
-            </View> 
-            
-            <View style={styles.carsData}>
-            
-                <View style={styles.cars}>
-                    <Image 
-                        style={styles.carIcon} 
-                        source={require('../img/vehicle-icon.png')}/>
-                    <Text>{this.state.cars.length} CARS</Text>
-                </View>             
-                
-                <View>
-                    {this.state.cars.map((car, i)=>{
-                        var firstline = `${car.make}, ${car.model}`;
-                        var secondline = `${car.owner}, ${car.year}`
-                        return (
-                            <View key={i}>
-                                <View>
-                                    <View style={styles.bulletCar}></View>
-                                </View>
-                                <View>
-                                    <Text>{firstline}</Text>
-                                    <Text>{secondline}</Text>
-                                </View>
-                            </View>
-                        );
-                    })}
-                </View>
-                   
             </View>
-            
-        </View>
+        </ScrollView>
         
-     </ScrollView>
+        <TouchableOpacity
+            onPress={this.housePage.bind(this)} 
+            style={buttonNext}>
+            <View style={btnContent}>
+                <Text style={btnNextText}>House</Text>
+                <Image source={require('../img/next-icon.png')}/>
+            </View>
+        </TouchableOpacity>
+            
+    </View>
     );
   }
 }
 
 var styles = StyleSheet.create({
+    scrollView: {
+        flex: 1
+    },
     wrapperContent: {
       flex: 1, 
       backgroundColor: backgroundClr,
@@ -190,7 +213,7 @@ var styles = StyleSheet.create({
     rowGender: {
         flexDirection: 'row',
         width: window.width * 0.90,
-        marginTop: 15,
+        marginTop: 20,
     },
     rowGenderSides: {
         flexDirection: 'row',
@@ -209,7 +232,57 @@ var styles = StyleSheet.create({
         color: 'white',
         textAlign: 'left',
         fontFamily: 'Avenir-Medium',
-        fontSize: 18,
-        paddingLeft: 10
+        fontSize: 22,
+        paddingLeft: 20
+    },
+    rowsPeopleDetail: {
+        width: window.width * 0.90,
+        flexDirection: 'column',
+        marginTop: 15,
+    },
+    wrapDetail: {
+        flexDirection: 'row',
+        flex: 1,
+        marginTop: 15,
+        alignItems: 'center'
+    },
+    leftDetail: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center'
+    },
+    bulletGender: {
+        width: 12,
+        height: 12,
+        borderRadius: 12/2,
+        backgroundColor: '#2D2BE6'
+    },
+    bulletF: {
+        backgroundColor: '#FF4BF4'
+    },
+    rightDetail: {
+        flex: 4,
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    nameage: {
+        fontSize: 22,
+        color: 'white',
+        textAlign: 'left',
+        fontFamily: 'Avenir-Medium',
+        fontStyle: 'italic',
+        paddingLeft: 17,
+    },
+    email: {
+        fontSize: 12,
+        color: 'white',
+        textAlign: 'left',
+        fontFamily: 'Avenir-Medium',
+        paddingLeft: 17,
+    },
+    carsData: {
+        flexDirection: 'column',
+        marginTop: 15,
+        width: window.width * 0.90
     },
 });
